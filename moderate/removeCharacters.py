b3c8cf1b26d222b331735ea8af7f6a13a61e0e97
JosejeSinohui <josejesinohui@gmail.com>
2016-05-25 18:03:28 -0700

    added compressedSequence.py

--- moderate/removeCharacters.py
+++ moderate/removeCharacters.py
@@ -1,16 +1,13 @@
-#Code Eval - Remove Characters
-#https://www.codeeval.com/open_challenges/13/
+# Code Eval - Remove Characters
+# https://www.codeeval.com/open_challenges/13/
 
 test_cases = ["how are you, abc",
               "hello world, def"]
 for test in test_cases:
     string = list(test.split(", ")[0])
     characters = test.split(", ")[1].strip()
-    
+
     for i in characters:
         while i in string:
             string.remove(i)
     print("".join(string))
-            
-    
-    
